b6dee69203468fde7a0eeee53ddd2fda136f8f6c
Tyler Confalone <tconfalone@gmail.com>
2018-05-19 17:54:31 -0700

    pop and push done

--- lab-tyler/_test_/list.test.js
+++ lab-tyler/_test_/list.test.js
@@ -5,35 +5,57 @@ let List = require('../lib/list.js');
 
 describe('List Module', () => {
 
-  it('push() returns the updated length of the list', () => {
+    describe('push()', () => {
 
+        it(' should return the updated length of the list', () => {
+
+            let myList = new List();
+            myList.push('FOO');
+
+            expect(myList.length).toBe(1);
+
+        });
+
+        it(' should add an element to the list', () => {
+            let myList = new List();
+            myList.push('foo');
+
+            expect(myList[myList.length - 1]).toEqual('foo');
+        });
+    })
+    describe('pop()', () => {
+        it('pop() returns the updated length of the list', () => {
+
+            let myList = new List();
+            myList.push('a');
+            myList.push('b');
+            myList.push('c');
+            myList.pop();
+
+            expect(myList.length).toBe(2);
+
+        });
+it('pop() rerturns last list item', () => {
     let myList = new List();
-    myList.push('FOO');
-
-    expect(myList.length).toBe(1);
-      
-  });
-
-  it('push() adds an element to the list', () => {
-    let myList = new List();
-    myList.push('foo');
-
-    expect(myList[myList.length - 1]).toEqual('foo');
-  });
-
-    it('push() returns the updated length of the list', () => {
-
-    let myList = new List();
-    myList.push('FOO');
-
-    expect(myList.length).toBe(1);
-      
-  });
-
-  it('push() adds an element to the list', () => {
-    let myList = new List();
-    myList.push('foo');
-
-    expect(myList[myList.length - 1]).toEqual('foo');
-  });
+    myList.push('a');
+    myList.push('b');
+    myList.push('c');
+    let popReturn = myList.pop();
+    expect(popReturn).toEqual('c');
 });
+});
+});
+
+
+// //test map
+// let list = new List ('apples','bananas');
+
+// list.map(fruit => fruit + ' sauce');
+
+// expect(babyFoods.toArray()).toEqual(['apples sauce','bananas sauce']);
+
+// expect(babyFoods.toEqual(new List(['apples sauce','bananas sauce']));
+
+// describe('For Each', () => {
+//     //make a counter to make sure callback function was called the right amount times
+// }
--- lab-tyler/lib/list.js
+++ lab-tyler/lib/list.js
@@ -2,18 +2,34 @@
 
 class List {
 
-  constructor() {
-    this.length = 0;
-  }
-  push(element) {
-    this[this.length++] = element;
-    return this.length;
-  }
-  pop(element) {
-    this[this.length.pop] = element;
-    return this.length;
-  }
-  
-}
+    constructor(array = []) {
+        this.length = array.length;
+        //want to populate list with item in array
+        for (let i = 0; i < array.length; i++) {
+            this[i] = array[i];
+        }
+    }
+    push(element) {
+        this[this.length++] = element;
+        return this.length;
+    }
+    pop() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        let lastI = this[this.length - 1];
+        delete this[this.length - 1];
+        this.length--;
+        return lastI;
+    }
 
+    toArray() {
+        let array = [];
+        //start at 0; finish at end(length)
+        for (let i = 0; i < array.length; i++) {
+            array[i] = this[i];
+        }
+        return array;
+    }
+}
 module.exports = List;
